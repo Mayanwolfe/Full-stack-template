706becb88c9d8cdffab51da1e471dc1554786076
Claire Bourdon <cebourdon625@gmail.com>
2022-06-22 20:43:40 -0400

    added port and Mongo connections

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express()
 const cors = require('cors')
 const MongoClient = require('mongodb').MongoClient
 require('dotenv').config()
+const PORT = 8000
 
 let db,
     dbConnectionString = process.env.DB_STRING,
@@ -15,3 +16,8 @@ MongoClient.connect(dbConnectionString)
         db = client.db(dbName)
         collection = db.collection('movies')
     })
+
+//PORT = 8000
+app.listen(process.env.PORT || PORT, () => {
+    console.log(`Server is running on port`)
+})
